e0ba2898a866893caa8080db727fd17d9e12e80e
Driver SPG <nick.nelson10@aol.com>
2015-12-16 09:38:32 -0800

    Initial Commit

---
+++ safe mass email/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  safe mass email
+//
+//  Created by Nicholas Nelson on 12/16/15.
+//  Copyright © 2015 Nicholas Nelson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
